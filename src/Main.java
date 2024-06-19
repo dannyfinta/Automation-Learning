1ca276cb2778c2c5ace9e1c639e9c10008ee5bfb
dannyfinta <danielfinta99@gmail.com>
2024-06-19 19:38:15 +0300

    Problema 6

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,45 @@ public class Main {
     public static void main(String[] args) {
         //problema1();
         //problema2();
-        problema3();
+        //problema3();
+        problema6();
+    }
 
+    public static void problema6(){
+        String t = "C";
+        System.out.println("Introduceti Tempertura in grade Celsius");
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextDouble() == true) {
+            double a = Double.parseDouble(scanner.nextLine());
+            System.out.println("Doriti sa transformati Temperatura in grade Fahrenheit sau Kelvin? : F/K");
+            Scanner doresc = new Scanner(System.in);
+            t = doresc.nextLine();
+            if (t.equals("f") | t.equals("F")) {
+                double gradef = 32 + a * 9 / 5;
+                System.out.println(a + " Grade C este echivaletul a " + gradef + " Grade Fahreinheit");
+
+            }
+                else {
+                    if (t.equals("k") | t.equals("K")){
+                        double gradek = 273.15 + a;
+                        System.out.println(a + " Grade C este echivaletul a " + gradek + " Grade Kelvin");
+
+                }
+                    else {
+                        System.out.println("Nu ati ales o conversie posibila");
+
+                    }
+
+            }
+        }
+        else{
+            System.out.println("Nu ati introdus o temperatura corecta");
+
+        }
 
     }
+
+
     public static void problema3() {
         String r = "Y";
         do {
